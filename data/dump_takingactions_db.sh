e36ba90f0afe0460371cb3fb67106edf1fbacc71
Server <server@server.com>
2013-12-02 12:00:50 +1100

    Modifying dump script so that it can be run on the server

--- data/dump_takingactions_db.sh
+++ data/dump_takingactions_db.sh
@@ -1,1 +1,1 @@
-pg_dump -U postgres takingactions > `date +%Y%m%d-%H%M%S`-db.sql
+sudo -u postgres pg_dump -t task -t task_undertaken -t task_contribution takingactions > `date +%Y%m%d-%H%M%S`-db.sql
