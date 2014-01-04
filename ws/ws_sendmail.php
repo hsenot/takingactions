31a3e8e8fd608f863877354f6cef20531a10a3c0
Server <server@server.com>
2014-01-04 13:20:50 +1100

    Added email templating via PHPmailer (dependency)

--- ws/ws_sendmail.php
+++ ws/ws_sendmail.php
@@ -1,32 +1,38 @@
 <?php
 
+	require '../../../phpmailer/PHPMailerAutoload.php';
+	$mail = new PHPMailer;
+
+	// Extracting parameter values
 	$p_to = $_REQUEST['to'];
 	$p_task_label = $_REQUEST['task_label'];
-	//$p_task_label = "Scrunch the political rhetoric in the form of instructions.";
 
-	$to = $p_to;
 	$subject = "Taking Action(s) - Task Reminder";
-	$body = "<html><body>Hi there,<br>".
-			"Thanks for taking part in our project.<br>".
-			"<br>".
-			"The task you selected is:<br>".
-			"<b>".$p_task_label."</b><br>".
-			"<br>".
-			"Please get in touch with us by replying to this email.<br>".
-			"<br>".
-			"Hope to be in touch soon!<br>".
-			"Cheers,<br>".
-			"<br>".
-			"Lizzy & Herve<br></body></html>";
-	$headers = "From: reminder@takingactions.org\r\n";
-	$headers .= "Bcc: s3426025@student.rmit.edu.au,lizzysampson@gmail.com\r\n";
-	$headers .= "X-Mailer: php\r\n";
-	$headers .= "Content-type: text/html\r\n";
 
-    header("Content-Type: text/html");
-	if (mail($to, $subject, $body, $headers)) {
-		echo('{"success":true,"message":"Message sent"}');
-	} else {
-		echo('{"success":false,"message":"Message delivery failed"}');
+	// Retrieving email template and substituting variables	
+	$body = file_get_contents('../email.html');
+	$body = str_replace('%task_label%',$p_task_label,$body);
+
+	// Other email parameters
+	$mail->From = 'reminder@takingactions.org';
+	$mail->FromName = 'Taking Action(s)';
+	$mail->addAddress($p_to);
+	$mail->addReplyTo('reply@takingactions.org', 'Taking Action(s)');
+	$mail->addBCC('s3426025@student.rmit.edu.au');
+	$mail->addBCC('lizzysampson@gmail.com');
+	$mail->isHTML(true);                                  // Set email format to HTML
+
+	$mail->Subject = $subject;
+	$mail->Body    = $body;
+	//$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+	header("Content-Type: text/html");
+	if(!$mail->send()) {
+		echo('{"success":false,"message":"Message delivery failed:'.$mail->ErrorInfo.'"}');	
 	}
+	else
+	{
+		echo('{"success":true,"message":"Message sent"}');
+	}
+
 ?>
